a969beeec169cf9922031e29201430d6deeb4e37
Carlifex <c.kluth@googlemail.com>
2026-07-13 06:47:32 +0000

    Kapitel/Lektionen trennen: Kapitelübersicht + nur Lektionen des gewählten Kapitels; einheitliche Begriffe (Kapitel/Lektion); 'Das lernst du'-Block (Grammatik+Vokabeln) unter jedem Einleitungstext; Cache v59

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 // werden automatisch nachgezogen. (CACHE-Version nur bumpen, wenn man alle
 // Caches hart leeren will.)
 
-const CACHE = 'gezellig-v58';
+const CACHE = 'gezellig-v59';
 const ASSETS = [
   './',
   './index.html',
